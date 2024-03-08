64fa0a40be3db93a802ba46721a638fd31987665
suimenkathemove <yoshiharu2580@gmail.com>
2024-03-08 12:02:37 +0900

    add base

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [
+    // "@suimenkathemove/frontend-eslint-config",
+    "./node_modules/@suimenkathemove/frontend-eslint-config",
+  ],
+  ignorePatterns: ["dist"],
+};
